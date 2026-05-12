3a0173435fe93bb93a33548c54fad6938c2c0742
alejandroms52 <alejandroms52@educastur.es>
2026-05-12 19:48:44 +0200

    Añadido esqueleto Spring Boot

---
+++ ciberAMS/src/test/java/com/ciberAMS/ciberAMS/CiberAmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciberAMS.ciberAMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiberAmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

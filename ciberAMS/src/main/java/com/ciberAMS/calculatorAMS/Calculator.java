a7ef4de22c73d9a5c3903e24e81b9fd77bd67480
alejandroms52 <alejandroms52@educastur.es>
2026-05-13 18:24:36 +0200

    Añadido Jenkinsfile

---
+++ ciberAMS/src/main/java/com/ciberAMS/calculatorAMS/Calculator.java
@@ -0,0 +1,9 @@
+package com.ciberAMS.calculatorAMS;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Calculator {
+	int sum(int a, int b) {
+		return a + b;
+	}
+}
--- ciberAMS/src/test/java/com/ciberAMS/calculatorAMS/CiberAmsApplicationTests.java
+++ ciberAMS/src/test/java/com/ciberAMS/calculatorAMS/CiberAmsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ciberAMS.ciberAMS;
+package com.ciberAMS.calculatorAMS;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
